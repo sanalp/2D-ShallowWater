2fb60f471472ff6cb1573987e2f35f4fb417b22d
Sanal Parameswaran <sanalceb@gmail.com>
2019-07-18 16:58:38 +0530

    Creted basic classes

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+/*!
+ * \file 	: <file_name.cpp>
+ * \author 	: Sanal Parameswaran <sanal@aero.iitb.ac.in>.
+ * \version : <version>
+ * \brief	: The code is implemented as a part of my Ph.D work.
+ */
